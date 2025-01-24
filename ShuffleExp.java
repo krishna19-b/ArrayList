898d3f99ea87cc6945a6aa8f8c8757b85202a7af
GURRAM KOUSHIK <Balakrishna>
2025-01-24 16:35:15 +0530

    Array List Shuffle Program

---
+++ ShuffleExp.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Arraylist{
+    public static void main(String args[]){
+        ArrayList<String> al=new ArrayList<>();
+            al.add("Black");
+            al.add("Blue");
+            al.add("Pink");
+            al.add("Green");
+            Collections.shuffle(al);
+            System.out.print(al);
+    }
+}
